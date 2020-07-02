c8d6f188dfccb583555bbef1f5e804b2289e7976
Luis <luiferlonbar@gmail.com>
2020-07-02 10:51:31 -0500

    Proyecto inicial curso Angular 9

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { $ } from 'protractor';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Lista de Ejercicios';
+}
+
